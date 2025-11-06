4022c15e6bbd85f82c4d1225b47c58d97f473bb2
nithishbachu <147524010+nithishbachu@users.noreply.github.com>
2025-11-06 12:55:23 +0530

    Add SQL queries for patient data with LIMIT and OFFSET
    Added SQL queries for patient data retrieval using LIMIT and OFFSET.

---
+++ Day_04/day4_queries.sql
@@ -0,0 +1,32 @@
+-- Day 3: LIMIT and OFFSET
+
+-- 1. Display the first 5 patients from the patients table.
+SELECT *
+FROM patients
+LIMIT 5;
+
+-- 2. Show patients 11-20 using OFFSET.
+SELECT *
+FROM patients
+LIMIT 10 OFFSET 10;
+
+-- 3. Get the 10 most recent patient admissions based on arrival_date.
+SELECT *
+FROM patients
+ORDER BY arrival_date DESC
+LIMIT 10;
+
+-- 4. Daily Challenge: Find the 3rd to 7th highest patient satisfaction scores from the patients table, showing patient_id, name, service and satisfaction. Display only these 5 records.
+SELECT patient_id, name AS patient_name, service, satisfaction AS satisfaction_score
+FROM patients
+ORDER BY satisfaction DESC
+LIMIT 5 OFFSET 2;
+
+
+
+
+
+
+
+
+
